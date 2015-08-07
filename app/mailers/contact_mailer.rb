de7dff4dd3570fa45f36a8738d176d3e44e717b3
Thomas Brown <tommy_brown@me.com>
2015-08-07 12:33:48 +0000

    contact_mailer.rb issues

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'tommy_brown@me.com'
   def contact_email(name, email, body)
-    @name = name #@instanceVars are assigned the values from vars assigned in contacts_controller (lines 9,10,11)
+    @name = name
     @email = email
-    @body  = body
-    mail(from: email, subject: 'Contact form message')
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
   end
 end
