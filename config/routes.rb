a56a0a3dca5778f923858cdd834dd2d988d44108
Thomas Brown <tommy_brown@me.com>
2015-08-17 08:41:55 +0000

    Added profiles controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   resources :contacts
   get '/about' => 'pages#about' #'pages#about' just means go to # (pages controller)-(about action)
   root 'pages#home'  # 'pages#home' just means go to # (pages controller)-(home action)
