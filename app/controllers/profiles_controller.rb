558fb490e9feccb8add4943f87fb6e96fcb4b391
Thomas Brown <tommy_brown@me.com>
2015-09-10 10:29:23 +0000

    Added form partial, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,8 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :only_current_user
-  
-  def new
+    def new
     # form where a user can fill out their own profile.
-    @user = User.find( params[:user_id] ) #User.find - from the db. params[:user_id] gets id from query string
-    @profile = Profile.new
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
   end
   def create 
     @user = User.find( params[:user_id] )
@@ -19,28 +16,12 @@ class ProfilesController < ApplicationController
   end
   
   def edit
-    @user = User.find( params[:user_id] )
-    @profile = @user.profile
-  end
-  
-  def update
-    @user = User.find( params[:user_id] )
-    @profile = @user.profile
-    if @profile.update_attributes(profile_params) #profile_params whitelisted below ;-D
-      flash[:success] = "Profile Updated!"
-      redirect_to user_path( params[:user_id] )
-    else
-      render action: :edit
-    end
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
   end
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-  
-  def only_current_user
-    @user = User.find( params[:user_id] )
-    redirect_to(root_url) unless @user == current_user
-  end
 end
